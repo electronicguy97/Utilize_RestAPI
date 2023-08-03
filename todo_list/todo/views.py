53de68e79ee8c12044768904a0e6201770e7759f
DoYeon Kim <103613730+electronicguy97@users.noreply.github.com>
2023-08-03 11:54:13 +0900

    Delete todo_list directory

--- todo_list/todo/views.py
+++
@@ -1,8 +0,0 @@
-# Todo 앱의 views.py 파일에 REST API 뷰를 생성
-from rest_framework import viewsets
-from .models import Todo
-from .serializers import TodoSerializer
-
-class TodoViewSet(viewsets.ModelViewSet):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
